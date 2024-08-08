a3640bcd157d9055e515293a4919d1f49d356eb5
Sai Nikith <sainikith0301@example.com>
2024-08-08 16:47:01 +0530

    "first commit"

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter,Roboto_Mono,Lusitana ,Roboto  } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+
+export const roboto = Roboto({
+    weight: '400',
+    subsets: ['latin'],
+    display: 'swap',
+  })
+export const lusitana =Lusitana({ weight: ['400', '700'],subsets:['latin']})
